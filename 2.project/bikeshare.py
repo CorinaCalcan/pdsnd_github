59ae1547f9740c8f11d4cb3001620bade064c3be
Corina <cc419c@czbrn4664.intl.att.com>
2020-11-05 16:03:08 +0100

    add statement to documentation

--- 2.project/bikeshare.py
+++ 2.project/bikeshare.py
@@ -6,6 +6,8 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+  #This is the 'new' refactoring of the cod- just kidding I like the code how it is
+
 def get_filters():
   """
   Asks user to specify a city, month, and day to analyze.
